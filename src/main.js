459179c91ac26fe22c083f53dd4fbd35bc8bfcf2
Edison <xutong1107@gmail.com>
2021-09-05 16:47:36 +0800

    test: Dynamic import

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
+// import Vconsole from 'vconsole'
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
+if (process.env.NODE_ENV === 'development') {
+  import('./vconsole')
+}
